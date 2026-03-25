47f03aa8bd2caf74b79a8459235234fa19cac1aa
uttupulusu Tarun <tarunuttupulusu@gmail.com>
2026-03-25 21:27:42 +0530

    added readme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
-
-// Configure global setting to use deployed backend URL if available
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
